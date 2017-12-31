c0c75c1a8211303480520f31fae482aa8ebe5ac0
up2code <uppban5@gmail.com>
2017-12-31 14:17:25 +0700

    Add placeholder images

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,5 +1,8 @@
 const images = {
     logo: require('./img/logo.png'),
+    unknownSong: require('./img/unknow_song.png'),
+    unknownAlbum: require('./img/unknow_album.png'),
+    unknownArtist: require('./img/unknow_artist.png'),
     getArtistUri: id => 'http://vocadb.net/Artist/Picture/' + id,
     getAlbumUri: id => 'http://vocadb.net/Album/CoverPicture/' + id
 };
