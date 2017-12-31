12419e4e92c2a4b2f3acd0b11bd6637df0f1cc36
up2code <uppban5@gmail.com>
2017-12-31 10:32:55 +0700

    Add sample album

--- src/modules/album/album.component.js
+++ src/modules/album/album.component.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 import { Container, ListItem, Content, Text, Button, Header, Left, Right, Grid, Row, Col, Title, Icon, StyleProvider, Body, Thumbnail } from 'native-base'
+import data from './../../sample/album'
+import images from './../../assets/images'
+import ListContainer from './../../components/ListContainer'
 
 class AlbumView extends React.Component {
 
@@ -13,7 +17,7 @@ class AlbumView extends React.Component {
                         </Button>
                     </Left>
                     <Body>
-                    <Title>Detail</Title>
+                        <Title>Detail</Title>
                     </Body>
                     <Right>
                         <Button transparent onPress={() => this.props.viewSearch()}>
@@ -23,6 +27,28 @@ class AlbumView extends React.Component {
 
                 </Header>
                 <Content>
+                    <Grid style={{ marginVertical: 16 }}>
+                        <Row>
+                            <Body>
+                                <Thumbnail square large source={{ uri: images.getAlbumUri(data.id) }} />
+                                <Text>{data.defaultName}</Text>
+                            </Body>
+                        </Row>
+                    </Grid>
+
+                    <ListContainer
+                        title='Artists'
+                        items={data.artists.map(a => a.artist)}
+                        displayItem={artist => (
+                            <TouchableOpacity style={{ margin: 8 }} key={artist.id}  onPress={() => console.log('press')}>
+                                <Thumbnail large source={{ uri: images.getArtistUri(artist.id) }}  />
+                                <Text>{artist.name}</Text>
+                                <Text note>{artist.artistType}</Text>
+                            </TouchableOpacity>
+
+                        )}
+                        horizontal={true}
+                    />
                 </Content>
             </Container>
         )
--- src/modules/album/album.container.js
+++ src/modules/album/album.container.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import AlbumView from './album.component'
-import { back } from "../nav/nav.action";
+import { back, viewSearch, viewArtist } from "../nav/nav.action";
 import { NavigationActions } from "react-navigation";
 
 const mapStateToProps = (state, props) => ({
@@ -9,7 +9,8 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = dispatch => ({
     back: () => dispatch(NavigationActions.back()),
-    viewSearch: () => dispatch(viewSearch())
+    viewSearch: () => dispatch(viewSearch()),
+    viewArtist: () => dispatch(viewArtist())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AlbumView)
