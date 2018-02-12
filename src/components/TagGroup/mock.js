b22eca0430cc332dc9828c60c94d27277fa98d0a
up2code <uppban5@gmail.com>
2018-02-12 20:35:37 +0700

    Add Tag group component

---
+++ src/components/TagGroup/mock.js
@@ -0,0 +1,112 @@
+export default {
+    "tags": [
+        {
+            "count": 1,
+            "tag": {
+                "additionalNames": "ギターソロ",
+                "id": 175,
+                "name": "guitar solo",
+                "urlSlug": "guitar-solo"
+            }
+        },
+        {
+            "count": 2,
+            "tag": {
+                "additionalNames": "Project Mirai DX",
+                "id": 272,
+                "name": "Project Mirai 2",
+                "urlSlug": "project-mirai-2"
+            }
+        },
+        {
+            "count": 1,
+            "tag": {
+                "additionalNames": "NSFW, explicit",
+                "id": 320,
+                "name": "mature content",
+                "urlSlug": "mature-content"
+            }
+        },
+        {
+            "count": 1,
+            "tag": {
+                "additionalNames": "pop-rock, guitar pop",
+                "id": 343,
+                "name": "pop rock",
+                "urlSlug": "pop-rock"
+            }
+        },
+        {
+            "count": 3,
+            "tag": {
+                "additionalNames": "",
+                "id": 353,
+                "name": "Project DIVA",
+                "urlSlug": "project-diva"
+            }
+        },
+        {
+            "count": 3,
+            "tag": {
+                "additionalNames": "",
+                "id": 355,
+                "name": "Project DIVA 2nd",
+                "urlSlug": "project-diva-2nd"
+            }
+        },
+        {
+            "count": 3,
+            "tag": {
+                "additionalNames": "",
+                "id": 359,
+                "name": "Project DIVA F 2nd",
+                "urlSlug": "project-diva-f-2nd"
+            }
+        },
+        {
+            "count": 1,
+            "tag": {
+                "additionalNames": "",
+                "id": 379,
+                "name": "romance",
+                "urlSlug": "romance"
+            }
+        },
+        {
+            "count": 1,
+            "tag": {
+                "additionalNames": "ジェイポップ",
+                "id": 1654,
+                "name": "J-pop",
+                "urlSlug": "j-pop"
+            }
+        },
+        {
+            "count": 3,
+            "tag": {
+                "additionalNames": "off vocal available, instrumental available",
+                "id": 3113,
+                "name": "karaoke available",
+                "urlSlug": "karaoke-available"
+            }
+        },
+        {
+            "count": 3,
+            "tag": {
+                "additionalNames": "",
+                "id": 3133,
+                "name": "Cinderella",
+                "urlSlug": "cinderella"
+            }
+        },
+        {
+            "count": 1,
+            "tag": {
+                "additionalNames": "",
+                "id": 3412,
+                "name": "explicit lyrics",
+                "urlSlug": "explicit-lyrics"
+            }
+        }
+    ]
+}
