bb226a982b8f4a020f0fa3ccddff5634f25b2cfb
up2code <uppban5@gmail.com>
2018-02-12 23:32:51 +0700

    Remove nativebase components

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import AppNavigationState from './src/AppNavigator'
 import AppNav from './src/AppNavigator'
 import { Provider } from 'react-redux'
 import store from './src/store'
 import { StyleProvider } from 'native-base'
-import getTheme from './native-base-theme/components';
-import material from './native-base-theme/variables/material';
 import StorybookUI from './storybook';
 
 const App = () => (
-    <StyleProvider  style={getTheme(material)}>
-            <Provider store={store}>
-                    <AppNav />
-            </Provider>
-    </StyleProvider>
+    <Provider store={store}>
+            <AppNav />
+    </Provider>
 )
 
 // export default App
