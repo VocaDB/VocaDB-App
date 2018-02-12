5c4fa8ebd0ea511df6e5bd7db6c1e7bff6e264cc
up2code <uppban5@gmail.com>
2018-02-12 23:32:03 +0700

    Add components to Song detail page

--- src/pages/SongDetail/component.js
+++ src/pages/SongDetail/component.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import { View, Text, Button, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import CenterView from './../../components/CenterView'
 import ArtistList from './../../components/ArtistList'
+import AlbumList from './../../components/AlbumList'
+import { Button } from 'react-native-elements'
+import Icon from './../../components/Icon'
+import TagGroup from './../../components/TagGroup'
+import Info from './../../components/Info'
+import ArtistRoleList from './../../components/ArtistRoleList'
+import WebLinkList from './../../components/WebLinkList'
 
 export default class SongDetailPage extends React.Component {
 
@@ -9,63 +16,44 @@ export default class SongDetailPage extends React.Component {
         this.props.fetchSong()
     }
 
-    renderSong () {
-        return (
-            <Text>{this.props.song.defaultName}</Text>
-        )
-    }
-
     render () {
 
         const song = this.props.song
         const artists = this.props.artists
 
-        console.log(artists)
+        const Section = props => (<View style={[{ marginVertical: 8},props.style]}>{props.children}</View>)
+
         return (
             <ScrollView style={{ flex: 1 }}>
-                <View style={{ height: 200, backgroundColor: '#64B5F6' }}>
-                    <CenterView style={{ backgroundColor: '#64B5F6' }}>
+                <View style={{ height: 180, backgroundColor: '#FFFFFF' }}>
+                    <CenterView style={{ backgroundColor: '#FFFFFF', justifyContent: 'space-around', padding: 10 }}>
                         <Image
                             style={{ width: 160 , height: 100 }}
                             source={{ uri: song.thumbUrl }}
                         />
-                        <Text>{song.defaultName}</Text>
+                        <Text style={{ fontSize: 18}}>{song.defaultName}</Text>
                         <Text>{song.artistString}</Text>
                     </CenterView>
                 </View>
-                <View style={{ height: 60, backgroundColor: '#2196F3' }}>
-                    <CenterView style={{ backgroundColor: '#2196F3', flexDirection: 'row' }}>
-                        <Text>Like</Text>
-                        <Text>Share</Text>
-                    </CenterView>
-                </View>
-                <View style={{ backgroundColor: '#B3E5FC' }}>
-                        <ArtistList artists={artists} />
-                </View>
-                <View style={{ height: 60, backgroundColor: '#1976D2' }}>
-                    <CenterView style={{ backgroundColor: '#1976D2' }}>
-                        <Text>Tags</Text>
-                    </CenterView>
-                </View>
-                <View style={{ height: 200, backgroundColor: '#7986CB' }}>
-                    <CenterView style={{ backgroundColor: '#7986CB' }}>
-                        <Text>General info</Text>
-                        <Text>Name</Text>
-                        <Text>Duration</Text>
-                        <Text>Published</Text>
-                        <Text>Addition date</Text>
-                    </CenterView>
-                </View>
-                <View style={{ height: 160, backgroundColor: '#3F51B5' }}>
-                    <CenterView style={{ backgroundColor: '#3F51B5' }}>
-                        <Text>Albums</Text>
-                    </CenterView>
-                </View>
-                <View style={{ height: 200, backgroundColor: '#303F9F' }}>
-                    <CenterView style={{ backgroundColor: '#303F9F' }}>
-                        <Text>Web links</Text>
-                    </CenterView>
-                </View>
+                <Section style={{ flex: 1, flexDirection: 'row' }}>
+                    <Icon name='heart' text='Favorite' />
+                    <Icon name='share' text='Share' />
+                    <Icon name='comments' text='Comment' />
+                    <Icon name='exclamation-circle'  text='Report' />
+                </Section>
+                <Section>
+                    <TagGroup tags={song.tags} max={5} />
+                </Section>
+                <Section>
+                    <ArtistList artists={artists} horizontal showHeader hideMoreButton title='Artists' />
+                </Section>
+                <Section>
+                    <AlbumList albums={song.albums} horizontal showHeader hideMoreButton title='Albums' />
+                </Section>
+                <Section>
+                    <WebLinkList webLinks={song.webLinks} category='Official' title='Official' />
+                    <WebLinkList webLinks={song.webLinks} category='Reference' title='Reference' />
+                </Section>
             </ScrollView>
         )
     }
