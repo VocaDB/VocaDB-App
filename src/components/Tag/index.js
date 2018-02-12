f96e1c72f146e7827f8a9203655b9f6100bd0e68
up2code <uppban5@gmail.com>
2018-02-12 20:36:01 +0700

    Update and style tag component

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -1,19 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 
 class Tag extends React.Component {
     render () {
         return (
-            <View>
-                <Text>{this.props.name}</Text>
-            </View>
+            <TouchableOpacity  onPress={this.props.onPress} style={
+                [{ backgroundColor: '#3949AB', padding: 8, flexDirection: 'row', justifyContent: 'space-between' },
+                this.props.style]}>
+                <Text style={{ color: 'white', fontWeight: 'bold' }}>{'#' + this.props.name}</Text>
+            </TouchableOpacity>
         )
     }
 }
 
 Tag.propTypes = {
-    name: PropTypes.string
+    name: PropTypes.string,
+    onPress: PropTypes.func
+}
+
+Tag.defaultProps = {
+    onPress: () => console.log('No action')
 }
 
 export default Tag;
