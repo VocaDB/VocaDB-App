7c2e13e93d360db95afa01e227e23220e971e2d4
up2code <uppban5@gmail.com>
2018-02-18 22:17:37 +0700

    Refactor schema

--- src/schema.js
+++ src/schema.js
@@ -1,16 +1,37 @@
 import { schema } from 'normalizr'
 
-export const entry = new schema.Entity('entries');
-export const album = new schema.Entity('albums');
-export const artist = new schema.Entity('artists');
-export const lyric = new schema.Entity('lyrics');
-export const webLink = new schema.Entity('webLinks');
-export const pv = new schema.Entity('pvs');
-export const tag = new schema.Entity('tags')
-export const artistRole = new schema.Entity('artistRoles', {
-    artist: artist
+const event = new schema.Entity('events')
+const entry = new schema.Entity('entries');
+const album = new schema.Entity('albums');
+const lyric = new schema.Entity('lyrics');
+const webLink = new schema.Entity('webLinks');
+const pv = new schema.Entity('pvs');
+const tag = new schema.Entity('tags')
+const artist = new schema.Entity('artists');
+const artistRole = new schema.Entity('artistRoles')
+const song = new schema.Entity('songs');
+
+artist.define({
+    relations: {
+        latestAlbums: [ album ],
+        latestEvents: [ event ],
+        latestSongs: [ song ],
+        popularAlbums: [ album ],
+        popularSongs: [ song ]
+    },
+    tags: [
+        {
+            tag: tag
+        }
+    ],
+    webLinks: [ webLink ]
 })
-export const song = new schema.Entity('songs', {
+
+artistRole.define({
+    artist
+})
+
+song.define({
     lyrics: [ lyric ],
     albums: [ album ],
     artists: [ artistRole ],
@@ -21,12 +42,12 @@ export const song = new schema.Entity('songs', {
             tag: tag
         }
     ]
-});
-export const event = new schema.Entity('events')
+})
 
 export default  {
     'songs': song,
     'events': event,
     'albums': album,
-    'entries': entry
+    'entries': entry,
+    'artists': artist
 }
