70b6f26beb453f82d0e8e1813f4403d886c8dad6
up2code <uppban5@gmail.com>
2018-02-22 16:23:32 +0700

    Styled home menu tab

--- src/pages/Home/component.js
+++ src/pages/Home/component.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button, ScrollView, RefreshControl } from 'react-native'
+import { View, Text, ScrollView, RefreshControl } from 'react-native'
 import SongList from '../../components/SongList'
 import AlbumList from '../../components/AlbumList'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
@@ -7,6 +7,10 @@ import CustomTabBar from './../../components/CustomTabBar'
 import mockEvent from './../../components/EventList/mock'
 import EventList from './../../components/EventList'
 import Content from './../../components/Content'
+import CenterView from './../../components/CenterView'
+import Icon from './../../components/Icon'
+import { ListItem, Button } from 'react-native-material-ui';
+import Theme from './../../theme'
 
 export default class HomePage extends React.Component {
 
@@ -35,7 +39,7 @@ export default class HomePage extends React.Component {
                     songs={this.props.recentSongs}
                     onPressItem={this.props.onPressSong}
                     hideMoreButton={true}
-                    footer={<Button style={{ backgroundColor: 'transparent' }} title='More' onPress={this.props.onPressMoreRecentSongs} />} />
+                    footer={<Button primary text="See more" />} />
                 <AlbumList title='Recent albums' max={10} horizontal={true} showHeader={true} albums={this.props.recentAlbums} onPressItem={this.props.onPressAlbum} />
             </Content>
         )
@@ -46,11 +50,43 @@ export default class HomePage extends React.Component {
             </ScrollView>
         )
 
+        const MenuTabPage =() => (
+            <View style={{ flex: 1 }}>
+                <CenterView>
+                    <Text style={[Theme.subhead,{ margin: 8 }]}>This app is still under development.</Text>
+                    <Button raised text="Sign in" />
+                </CenterView>
+                <View style={{ flex: 1, backgroundColor: 'white' }}>
+                    <ListItem
+                        leftElement={<Icon name='ios-settings' />}
+                        centerElement={{
+                            primaryText: 'Settings',
+                        }}
+                        onPress={() => {}}
+                    />
+                    <ListItem
+                        leftElement={<Icon name='ios-lock' />}
+                        centerElement={{
+                            primaryText: 'Terms & privacy policy',
+                        }}
+                        onPress={() => {}}
+                    />
+                    <ListItem
+                        leftElement={<Icon name='ios-help-circle' />}
+                        centerElement={{
+                            primaryText: 'Help & feedback',
+                        }}
+                        onPress={() => {}}
+                    />
+                </View>
+            </View>
+        )
+
         return (
             <ScrollableTabView renderTabBar={() => <CustomTabBar />}>
                 <HomeTabPage tabLabel="ios-home" />
                 <EventTabPage tabLabel="ios-calendar"></EventTabPage>
-                <View tabLabel="ios-menu"><Text>Menu</Text></View>
+                <MenuTabPage tabLabel="ios-menu"></MenuTabPage>
             </ScrollableTabView>
         )
     }
