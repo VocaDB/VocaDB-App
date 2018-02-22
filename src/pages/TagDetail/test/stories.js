bc4f356162baa70efd93ec8e5cf2a00fcc8099be
up2code <uppban5@gmail.com>
2018-02-22 23:22:01 +0700

    Fix stories file

--- src/pages/TagDetail/test/stories.js
+++ src/pages/TagDetail/test/stories.js
@@ -4,12 +4,10 @@ import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 
 import TagPage from '../component'
-import mockTag from './mock'
 
 storiesOf('Pages/Tag', module)
     .add('normal', () => (
         <TagPage
-            tag={mockTag}
             fetchTag={action('Fetch tag')}
         />
     ));
