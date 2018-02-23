0865f6cf64b80bed7134e39f7242f833c5746d9b
up2code <uppban5@gmail.com>
2018-02-23 13:41:52 +0700

    Build own wrapper search bar component

---
+++ src/components/SearchBar/stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+
+import SearchBar from './index'
+
+storiesOf('SearchBar', module)
+    .add('general', () => (
+        <SearchBar
+            onPressClear={action('Press clear')}
+            onLeftElementPress={action('Press back')}
+            onChangeText={action('Input')}
+            onRightElementPress={action('Press right element')}
+        />
+    ))
+    .add('with text', () => (
+        <SearchBar
+            text='something'
+            onPressClear={action('Press clear')}
+            onLeftElementPress={action('Press back')}
+            onChangeText={action('Input')}
+            onRightElementPress={action('Press right element')}
+        />
+    ));
