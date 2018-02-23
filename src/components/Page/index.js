73d04bd6da85740e4bf29dd85a4fdd140153b8e1
up2code <uppban5@gmail.com>
2018-02-23 13:41:15 +0700

    Fix Page component theme

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import { View, Text, StatusBar, Platform } from 'react-native'
-import { statusBarColor } from './../../theme'
+import Theme from './../../theme'
 import { Constants } from 'expo'
 import { SafeAreaView } from 'react-navigation'
-import { rootBackgroundColor } from './../../theme'
 
 class Page extends React.Component {
     render () {
         if(Platform.OS === 'ios') {
             return (
-                <SafeAreaView style={{ flex: 1, backgroundColor: rootBackgroundColor }}>
+                <SafeAreaView style={{ flex: 1, backgroundColor: Theme.rootBackgroundColor }}>
                     {this.props.children}
                 </SafeAreaView>
             )
         } else {
             return (
                 <View style={{ flex: 1 }}>
-                    <View style={{ height: Constants.statusBarHeight, backgroundColor: statusBarColor }}></View>
+                    <View style={{ height: Constants.statusBarHeight, backgroundColor: Theme.statusBarColor }}></View>
                     <View style={{ flex: 1, backgroundColor: rootBackgroundColor }}>
                         {this.props.children}
                     </View>
