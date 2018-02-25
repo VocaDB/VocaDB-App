1316720bd3fe9eeb25eeeed43c425099d3ac9703
up2code <uppban5@gmail.com>
2018-02-25 14:17:40 +0700

    Add some media related icon image

--- src/assets/images.js
+++ src/assets/images.js
@@ -4,6 +4,9 @@ const images = {
     unknownAlbum: require('./img/unknow_album.png'),
     unknownArtist: require('./img/unknow_artist.png'),
     placeholderUri: 'http://via.placeholder.com/90x90?text=NO_IMAGE',
+    iconYoutube: require('./img/icon_youtube.png'),
+    iconSoundcloud: require('./img/icon_soundcloud.png'),
+    iconNicoNicoDouga: require('./img/icon_niconicodouga.jpg'),
     getArtistUri: id => 'http://vocadb.net/Artist/Picture/' + id,
     getAlbumUri: id => 'http://vocadb.net/Album/CoverPicture/' + id
 };
