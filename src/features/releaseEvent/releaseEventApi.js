79bc8246cdeb9e435a8f9a1cbe91a7f6b3044c35
Augsorn Chanklad <uppban5@gmail.com>
2018-03-18 23:17:09 +0700

    Change condition latest event date

--- src/features/releaseEvent/releaseEventApi.js
+++ src/features/releaseEvent/releaseEventApi.js
@@ -1,7 +1,7 @@
 import api from '../../common/api'
 
 const releaseEventApi = {
-    getRecentReleaseEvents: () => api.events.find({ 'sort': 'AdditionDate', 'fields': 'MainPicture' }),
+    getRecentReleaseEvents: () => api.events.find({ 'sort': 'Date', 'fields': 'MainPicture' }),
     getReleaseEvent: id => api.events.get(id)
 }
 
