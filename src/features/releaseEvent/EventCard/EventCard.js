a8d7b9733a20815a822a1af3c54b6f0663bc8712
Augsorn Chanklad <uppban5@gmail.com>
2018-03-18 23:13:16 +0700

    Add event card component

---
+++ src/features/releaseEvent/EventCard/EventCard.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Image, View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types';
+import images from '../../../common/assets/images'
+import Theme from '../../../theme'
+import moment from 'moment'
+
+class EventCard extends React.Component {
+    render () {
+        return (
+            <TouchableOpacity onPress={this.props.onPress} style={[styles.container]}>
+                <Image style={[styles.coverImage]} source={{ uri: this.props.thumbnail }} />
+                <View style={[styles.infoContainer]}>
+                    <Text style={Theme.subheadWhite}>{moment(this.props.date).format('dddd, MMMM Do YYYY')}</Text>
+                    <Text style={Theme.headlineWhite}>{this.props.name}</Text>
+                    <Text style={Theme.headlineWhite}>{this.props.location}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+}
+
+EventCard.propTypes = {
+    name: PropTypes.string,
+    image: PropTypes.string,
+    location: PropTypes.string,
+    date: PropTypes.string
+};
+
+EventCard.defaultProps = {
+    thumbnail: 'http://via.placeholder.com/350x150/000000/ffffff?text=EVENT'
+};
+
+const styles =  StyleSheet.create({
+    container: {
+        width: 320,
+        height: 180,
+        backgroundColor: 'black',
+        justifyContent: 'flex-end',
+        margin: 8
+    },
+    coverImage: {
+        flex: 1,
+        opacity: 0.57
+    },
+    infoContainer: {
+        backgroundColor:'transparent',
+        position: 'absolute',
+        padding: 8
+    }
+})
+
+
+export default EventCard;
