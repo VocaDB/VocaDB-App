8555dc933ff50e27db425835b80c7ddecf496477
Augsorn Chanklad <uppban5@gmail.com>
2018-03-19 18:56:59 +0700

    Add Home tab component

---
+++ src/features/main/HomeTab/HomeTab.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Avatar } from 'react-native-material-ui';
+import PropTypes from 'prop-types'
+import FeatureList from './../FeatureList'
+import Theme from '../../../theme'
+import Content from '../../../components/Content'
+import Divider from '../../../components/Divider'
+import SongCard from '../../song/SongCard'
+import AlbumCard from '../../album/AlbumCard'
+import EventCard from '../../releaseEvent/EventCard'
+
+
+class HomeTab extends React.Component {
+    render () {
+
+        const MenuIcon = (props) => (
+            <TouchableOpacity style={{ alignItems: 'center' }} onPress={props.onPress}>
+                <Avatar icon={props.icon} />
+                <Text style={Theme.caption}>{props.text}</Text>
+            </TouchableOpacity>
+        )
+
+        const renderSongCard = song => (
+            <SongCard key={song.id}
+                      id={song.id}
+                      name={song.defaultName}
+                      artist={song.artistString}
+                      image={song.thumbUrl}
+                      onPress={() => this.props.onPressSong(song)} />
+        )
+
+        const renderAlbumCard = album => (
+            <AlbumCard key={album.id}
+                       id={album.id}
+                       name={album.defaultName}
+                       onPress={() => this.props.onPressAlbum(album)} />
+        )
+
+        const renderEventCard = event => {
+            const thumbnailUrl = (event.mainPicture) ? event.mainPicture.urlThumb.replace('mainThumb', 'mainOrig') : undefined
+            return (
+                <EventCard  key={event.id}
+                            name={event.name}
+                            thumbnail={thumbnailUrl}
+                            location={event.venueName}
+                            date={event.date}
+                            onPress={() => this.props.onPressEvent(event)} />
+            )
+        }
+
+
+        const renderFeatureList = (title, items, renderItem, onPressMore) => (
+            <FeatureList
+                title={title}
+                items={items.map(renderItem)}
+                onPressMore={this.props.onPressMoreRecentSongs} />
+        )
+
+        return (
+            <Content>
+                <View style={[styles.menuContainer]}>
+                    <MenuIcon icon='music-note' text='Song' onPress={this.props.onPressSongSearch} />
+                    <MenuIcon icon='person' text='Artist' onPress={this.props.onPressArtistSearch} />
+                    <MenuIcon icon='album' text='Album' onPress={this.props.onPressAlbumSearch} />
+                    <MenuIcon icon='event' text='Event' onPress={this.props.onPressEventSearch} />
+                </View>
+
+                <Divider height={14} />
+
+                <View style={{ paddingBottom: 16}}>
+                    {renderFeatureList('Recent songs', this.props.recentSongs, renderSongCard, this.props.onPressMoreRecentSongs)}
+
+                    <Divider height={14} />
+
+                    {renderFeatureList('Recent albums', this.props.recentAlbums, renderAlbumCard, this.props.onPressMoreRecentAlbums)}
+
+                    <Divider height={14} />
+
+                    {renderFeatureList('Latest events', this.props.latestEvents, renderEventCard, this.props.onPressMoreLatestEvent)}
+                </View>
+            </Content>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    menuContainer: {
+        flexDirection: 'row',
+        height: 72,
+        justifyContent: 'space-around',
+        marginVertical: 16
+    }
+})
+
+HomeTab.propTypes = {
+    onPressSongSearch: PropTypes.func,
+    onPressArtistSearch: PropTypes.func,
+    onPressAlbumSearch: PropTypes.func,
+    onPressEventSearch: PropTypes.func,
+    onPressMoreRecentSongs: PropTypes.func,
+    onPressMoreRecentAlbums: PropTypes.func,
+    onPressMoreLatestEvent: PropTypes.func
+}
+
+HomeTab.defaultProps = {
+    recentSongs: [],
+    recentAlbums: [],
+    latestEvents: []
+}
+
+export default HomeTab
--- src/features/main/Main/Main.js
+++ src/features/main/Main/Main.js
@@ -1,16 +1,7 @@
 import React from 'react'
-import { View, Text, ScrollView, RefreshControl, TouchableOpacity } from 'react-native'
-import SongList from '../../song/SongList'
-import SongCard from '../../song/SongCard'
-import AlbumCard from '../../album/AlbumCard'
-import EventCard from '../../releaseEvent/EventCard'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
 import CustomTabBar from '../../../components/CustomTabBar'
-import Content from '../../../components/Content'
-import Divider from '../../../components/Divider'
-import Theme from '../../../theme'
-import { Avatar } from 'react-native-material-ui';
-import FeatureList from './../FeatureList'
+import HomeTab from './../HomeTab'
 import MenuTab from './../MenuTab'
 import FollowedTab from './../FollowedTab'
 
@@ -29,91 +20,20 @@ export default class Main extends React.Component {
 
     render () {
 
-        const renderSongCard = song => (
-            <SongCard key={song.id}
-                      id={song.id}
-                      name={song.defaultName}
-                      artist={song.artistString}
-                      image={song.thumbUrl}
-                      onPress={() => this.props.onPressSong(song)} />
-        )
-
-        const renderAlbumCard = album => (
-            <AlbumCard key={album.id}
-                       id={album.id}
-                       name={album.defaultName}
-                       onPress={() => this.props.onPressAlbum(album)} />
-        )
-
-        const renderEventCard = event => {
-            const thumbnailUrl = (event.mainPicture) ? event.mainPicture.urlThumb.replace('mainThumb', 'mainOrig') : undefined
-            return (
-                <EventCard  key={event.id}
-                            name={event.name}
-                            thumbnail={thumbnailUrl}
-                            location={event.venueName}
-                            date={event.date}
-                            onPress={() => this.props.onPressEvent(event)} />
-            )
-        }
-
-
-        const renderFeatureList = (title, items, renderItem, onPressMore) => (
-            <FeatureList
-                title={title}
-                items={items.map(renderItem)}
-                onPressMore={this.props.onPressMoreRecentSongs} />
-        )
-
-        const HomeTabPage = () => (
-            <Content refreshControl={
-                <RefreshControl
-                    refreshing={this.props.refreshing}
-                    onRefresh={this.refresh.bind(this)}
-                />}>
-
-                <View style={{ marginVertical: 16 }}>
-
-                    <View style={{ flexDirection: 'row', height: 72, justifyContent: 'space-around' }}>
-                        <TouchableOpacity style={{ alignItems: 'center' }} onPress={this.props.onPressSongSearch}>
-                            <Avatar icon='music-note' />
-                            <Text style={Theme.caption}>Songs</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{ alignItems: 'center' }} onPress={this.props.onPressArtistSearch}>
-                            <Avatar icon='person' />
-                            <Text style={Theme.caption}>Artist</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => {}}>
-                            <Avatar icon='album' />
-                            <Text style={Theme.caption}>Album</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => {}}>
-                            <Avatar icon='event' />
-                            <Text style={Theme.caption}>Event</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
-                <Divider height={14} />
-
-                <View style={{ paddingBottom: 16}}>
-                    {renderFeatureList('Recent songs', this.props.recentSongs, renderSongCard, this.props.onPressMoreRecentSongs)}
-
-                    <Divider height={14} />
-
-                    {renderFeatureList('Recent albums', this.props.recentAlbums, renderAlbumCard, this.props.onPressMoreRecentSongs)}
-
-                    <Divider height={14} />
-
-                    {renderFeatureList('Latest events', this.props.latestEvents, renderEventCard, this.props.onPressMoreRecentSongs)}
-                </View>
-
-            </Content>
-        )
-
         return (
             <ScrollableTabView renderTabBar={() => <CustomTabBar />}>
-                <HomeTabPage tabLabel="ios-home" />
+                <HomeTab
+                    tabLabel="ios-home"
+                    recentSongs={this.props.recentSongs}
+                    recentAlbums={this.props.recentAlbums}
+                    latestEvents={this.props.latestEvents}
+                    onPressSongSearch={this.props.onPressSongSearch}
+                    onPressArtistSearch={this.props.onPressArtistSearch}
+                    onPressAlbumSearch={this.props.onPressAlbumSearch}
+                    onPressEventSearch={this.props.onPressEventSearch}
+                    onPressMoreRecentSongs={this.props.onPressMoreRecentSongs}
+                    onPressMoreRecentAlbums={this.props.onPressMoreRecentAlbums}
+                    onPressMoreLatestEvent={this.props.onPressMoreLatestEvent} />
                 <FollowedTab tabLabel="ios-heart" songs={this.props.followedSongs} onPressSong={this.props.onPressSong} />
                 <MenuTab tabLabel="ios-menu"></MenuTab>
             </ScrollableTabView>
