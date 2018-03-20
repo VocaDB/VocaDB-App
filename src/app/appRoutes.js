a10692e3fe3dd0381fa66fc17823b8689bb510a8
Augsorn Chanklad <uppban5@gmail.com>
2018-03-20 21:53:33 +0700

    Add route names constant file

---
+++ src/app/appRoutes.js
@@ -0,0 +1,16 @@
+const ROUTES = {
+    Main: 'Main',
+    SongDetail: 'SongDetail',
+    ArtistDetail: 'ArtistDetail',
+    AlbumDetail: 'AlbumDetail',
+    AlbumSearch: 'AlbumSearch',
+    Search: 'Search',
+    SongList: 'SongList',
+    SongFilter: 'SongFilter',
+    ArtistList: 'ArtistList',
+    TagDetail: 'TagDetail',
+    EventDetail: 'EventDetail',
+    FollowedArtists: 'FollowedArtists'
+}
+
+export default ROUTES
