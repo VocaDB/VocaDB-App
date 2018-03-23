04bce19f89fb9a413d77658c2772624370b6f6f4
Augsorn Chanklad <uppban5@gmail.com>
2018-03-23 17:27:29 +0700

    Fixed #9 app exit when press back in android

--- src/app/AppWithNavigationState.js
+++ src/app/AppWithNavigationState.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { BackHandler } from "react-native";
 import AppNavigator from './../AppNavigator'
 import {
     addNavigationHelpers,
+    NavigationActions
 } from 'react-navigation';
 import {
     createReduxBoundAddListener,
@@ -16,6 +18,23 @@ export const navMiddleware = createReactNavigationReduxMiddleware(
 const addListener = createReduxBoundAddListener("root");
 
 class App extends React.Component {
+
+    componentDidMount() {
+        BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+    }
+    componentWillUnmount() {
+        BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+    }
+    onBackPress = () => {
+        const { dispatch, nav } = this.props;
+        if (nav.index === 0) {
+            return false;
+        }
+        dispatch(NavigationActions.back());
+        return true;
+    };
+
+
     render() {
         return (
             <AppNavigator navigation={addNavigationHelpers({
