444e4bc74654e4909673acec5f85de4a194ada99
Augsorn Chanklad <uppban5@gmail.com>
2018-03-25 18:42:05 +0700

    Add song horizontal list component

---
+++ src/features/song/SongHorizontalList/SongHorizontalList.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { View, Text, ScrollView, StyleSheet, FlatList } from 'react-native'
+import PropTypes from 'prop-types'
+import Theme from '../../../theme'
+import { Button } from 'react-native-material-ui';
+import FeatureList from './../../main/FeatureList'
+import SongCard from './../SongCard'
+
+class SongHorizontalList extends React.Component {
+    render () {
+
+        const renderSongCard = song => (
+            <SongCard key={song.id}
+                      id={song.id}
+                      name={song.defaultName}
+                      artist={song.artistString}
+                      image={song.thumbUrl}
+                      pvs={song.pvs}
+                      onPress={() => this.props.onPressSong(song)} />
+        )
+
+        return (
+            <FeatureList
+                title={this.props.title}
+                items={this.props.songs}
+                displayMoreButton={false}
+                renderItem={renderSongCard} />
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white'
+    },
+    wrapContainer: {
+        margin: 8
+    },
+    headerContainer: {
+        margin: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    contentContainer: {
+        padding: 8,
+        margin: 8
+    }
+});
+
+SongHorizontalList.propTypes = {
+    title: PropTypes.string,
+    songs: PropTypes.array,
+    renderItem: PropTypes.func,
+    onPressMore: PropTypes.func,
+    onPressItem: PropTypes.func,
+}
+
+SongHorizontalList.defaultProps = {
+    title: 'Songs',
+    songs: [],
+    onPressItem: () => console.log('Press item'),
+    onPressMore: () => console.log('Press more')
+}
+
+export default SongHorizontalList
