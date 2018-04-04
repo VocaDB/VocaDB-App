0ec2afcd5ddd4d8a15ae2d995bb27e754bda8dd8
Augsorn Chanklad <uppban5@gmail.com>
2018-04-04 14:11:27 +0700

    Add caching request

--- src/common/api/request.js
+++ src/common/api/request.js
@@ -1,19 +1,32 @@
 import axios from 'axios'
 import config from '../constants/config'
+import cache from 'memory-cache'
+import hash from 'object-hash'
 
 const axiosAPI = axios.create({
     baseURL: config.host
 });
 
+const toMinute = (ms) => ms * 60000
+
 const request = (endpoint, params, mockData) => {
-
-    console.log('endpoint : ' + endpoint)
-    console.log(params)
-
     if(config.useMock) {
         return new Promise((resolve, reject) => setTimeout(() => resolve(mockData), 500))
     }
-    return axiosAPI.get(endpoint, { params }).then(response => response.data)
+
+    const requestHash = hash({ e: endpoint, p: params })
+    const responseCache = cache.get(requestHash)
+
+    if(responseCache) {
+       return responseCache;
+    }
+
+    return axiosAPI.get(endpoint, { params })
+        .then(response => {
+            cache.put(requestHash, response.data, toMinute(5))
+            return response.data
+        })
+        .catch(error => console.log(error))
 }
 
 export default request
