288e9449b70bd6cb3fe85433e0a18087a097ac7b
Augsorn Chanklad <uppban5@gmail.com>
2018-04-05 20:45:48 +0700

    Fix songs HOC

--- src/features/song/songHOC.js
+++ src/features/song/songHOC.js
@@ -6,9 +6,8 @@ const createList = (ItemComponent) => (props) => (  <FlatList
     data={props.data}
     keyExtractor={item => item.id}
     renderItem={({ item }) => {
-        const imageUrl = (item.thumbUrl) ? item.thumbUrl : 'http://via.placeholder.com/350x150/000000/ffffff?text=NO_IMAGE';
         return (<ItemComponent key={item.id}
-                               image={imageUrl}
+                               image={item.image}
                                name={item.defaultName}
                                artist={item.artistString}
                                dateTime={item.createDate}
