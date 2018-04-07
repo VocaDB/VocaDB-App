64b020db78e7067df91e81b0ed8c81519f2d34cf
Augsorn Chanklad <uppban5@gmail.com>
2018-04-07 18:22:45 +0700

    Cleanup package

--- src/app/appStore.js
+++ src/app/appStore.js
@@ -1,12 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducers from './appReducer'
-import thunkMiddleware from 'redux-thunk'
-import loggerMiddleware from 'redux-logger'
+// import loggerMiddleware from 'redux-logger'
 import { navMiddleware } from './AppWithNavigationState'
 import createSagaMiddleware from 'redux-saga'
 import saga from './appSagas'
-import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
-
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 
@@ -27,7 +24,6 @@ export default () => {
         applyMiddleware(
             // loggerMiddleware,
             navMiddleware,
-            thunkMiddleware,
             sagaMiddleware)
     )
     let persistor = persistStore(store)
