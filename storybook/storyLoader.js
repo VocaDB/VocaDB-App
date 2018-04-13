337164ef9e0594ecbb249302d5009bf9b48d3b8e
Augsorn Chanklad <uppban5@gmail.com>
2018-04-13 21:16:51 +0700

    Add story loader

---
+++ storybook/storyLoader.js
@@ -0,0 +1,124 @@
+
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/Cover/test/stories');
+  require('../src/components/Divider/test/stories');
+  require('../src/components/Empty/test/stories');
+  require('../src/components/Icon/test/stories');
+  require('../src/components/Info/test/stories');
+  require('../src/components/MenuItem/test/stories');
+  require('../src/components/SearchBar/test/stories');
+  require('../src/features/album/AlbumCard/test/stories');
+  require('../src/features/album/AlbumDetail/test/stories');
+  require('../src/features/album/AlbumFilter/test/stories');
+  require('../src/features/album/AlbumGridView/test/stories');
+  require('../src/features/album/AlbumHorizontalList/test/stories');
+  require('../src/features/album/AlbumList/test/stories');
+  require('../src/features/album/AlbumRow/test/stories');
+  require('../src/features/album/AlbumSearch/test/stories');
+  require('../src/features/artist/ArtistDetail/test/stories');
+  require('../src/features/artist/ArtistList/test/stories');
+  require('../src/features/artist/ArtistRow/test/stories');
+  require('../src/features/artist/ArtistSearch/test/stories');
+  require('../src/features/artist/FollowedArtists/test/stories');
+  require('../src/features/artistRole/ArtistRoleList/test/stories');
+  require('../src/features/entry/EntryList/test/stories');
+  require('../src/features/entry/EntryRow/test/stories');
+  require('../src/features/entry/EntrySearch/test/stories');
+  require('../src/features/lyric/LyricContent/test/stories');
+  require('../src/features/lyric/LyricGroup/test/stories');
+  require('../src/features/main/FeatureList/test/stories');
+  require('../src/features/main/FollowedTab/test/stories');
+  require('../src/features/main/HomeTab/test/stories');
+  require('../src/features/main/Main/test/stories');
+  require('../src/features/main/MenuTab/test/stories');
+  require('../src/features/pv/PVLIst/test/stories');
+  require('../src/features/pv/PVRow/test/stories');
+  require('../src/features/releaseEvent/EventCard/test/stories');
+  require('../src/features/releaseEvent/EventDetail/test/stories');
+  require('../src/features/releaseEvent/EventList/test/stories');
+  require('../src/features/releaseEvent/EventRow/test/stories');
+  require('../src/features/song/FavoriteSongs/test/stories');
+  require('../src/features/song/SongCard/test/stories');
+  require('../src/features/song/SongDetail/test/stories');
+  require('../src/features/song/SongFilter/test/stories');
+  require('../src/features/song/SongHorizontalList/test/stories');
+  require('../src/features/song/SongList/test/stories');
+  require('../src/features/song/SongRow/test/stories');
+  require('../src/features/song/SongSearch/test/stories');
+  require('../src/features/song/SongWithParams/test/stories');
+  require('../src/features/tag/Tag/test/stories');
+  require('../src/features/tag/TagDetail/test/stories');
+  require('../src/features/tag/TagGroup/test/stories');
+  require('../src/features/track/Track/test/stories');
+  require('../src/features/track/TrackList/test/stories');
+  require('../src/features/webLink/WebLink/test/stories');
+  require('../src/features/webLink/WebLinkList/test/stories');
+  
+}
+
+const stories = [
+  '../src/components/Cover/test/stories',
+  '../src/components/Divider/test/stories',
+  '../src/components/Empty/test/stories',
+  '../src/components/Icon/test/stories',
+  '../src/components/Info/test/stories',
+  '../src/components/MenuItem/test/stories',
+  '../src/components/SearchBar/test/stories',
+  '../src/features/album/AlbumCard/test/stories',
+  '../src/features/album/AlbumDetail/test/stories',
+  '../src/features/album/AlbumFilter/test/stories',
+  '../src/features/album/AlbumGridView/test/stories',
+  '../src/features/album/AlbumHorizontalList/test/stories',
+  '../src/features/album/AlbumList/test/stories',
+  '../src/features/album/AlbumRow/test/stories',
+  '../src/features/album/AlbumSearch/test/stories',
+  '../src/features/artist/ArtistDetail/test/stories',
+  '../src/features/artist/ArtistList/test/stories',
+  '../src/features/artist/ArtistRow/test/stories',
+  '../src/features/artist/ArtistSearch/test/stories',
+  '../src/features/artist/FollowedArtists/test/stories',
+  '../src/features/artistRole/ArtistRoleList/test/stories',
+  '../src/features/entry/EntryList/test/stories',
+  '../src/features/entry/EntryRow/test/stories',
+  '../src/features/entry/EntrySearch/test/stories',
+  '../src/features/lyric/LyricContent/test/stories',
+  '../src/features/lyric/LyricGroup/test/stories',
+  '../src/features/main/FeatureList/test/stories',
+  '../src/features/main/FollowedTab/test/stories',
+  '../src/features/main/HomeTab/test/stories',
+  '../src/features/main/Main/test/stories',
+  '../src/features/main/MenuTab/test/stories',
+  '../src/features/pv/PVLIst/test/stories',
+  '../src/features/pv/PVRow/test/stories',
+  '../src/features/releaseEvent/EventCard/test/stories',
+  '../src/features/releaseEvent/EventDetail/test/stories',
+  '../src/features/releaseEvent/EventList/test/stories',
+  '../src/features/releaseEvent/EventRow/test/stories',
+  '../src/features/song/FavoriteSongs/test/stories',
+  '../src/features/song/SongCard/test/stories',
+  '../src/features/song/SongDetail/test/stories',
+  '../src/features/song/SongFilter/test/stories',
+  '../src/features/song/SongHorizontalList/test/stories',
+  '../src/features/song/SongList/test/stories',
+  '../src/features/song/SongRow/test/stories',
+  '../src/features/song/SongSearch/test/stories',
+  '../src/features/song/SongWithParams/test/stories',
+  '../src/features/tag/Tag/test/stories',
+  '../src/features/tag/TagDetail/test/stories',
+  '../src/features/tag/TagGroup/test/stories',
+  '../src/features/track/Track/test/stories',
+  '../src/features/track/TrackList/test/stories',
+  '../src/features/webLink/WebLink/test/stories',
+  '../src/features/webLink/WebLinkList/test/stories',
+  
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
