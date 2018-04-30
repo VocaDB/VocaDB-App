f431e51955f3c51a58d41741bcc41dce3d80b0f7
Augsorn Chanklad <uppban5@gmail.com>
2018-04-30 11:10:10 +0700

    Change favorite albums route
    For member route will be next phase

--- src/features/main/Main/index.js
+++ src/features/main/Main/index.js
@@ -71,7 +71,7 @@ const mapDispatchToProps = (dispatch, props) => ({
     }),
     onPressMenuFollowArtists: () => props.navigation.navigate(Routes.FollowedArtists),
     onPressMenuFavoriteSongs: () => props.navigation.navigate(Routes.FavoriteSongs),
-    onPressMenuFavoriteAlbums: () => props.navigation.navigate(Routes.MyAlbums),
+    onPressMenuFavoriteAlbums: () => props.navigation.navigate(Routes.FavoriteAlbums),
     onFilterByChanged: value => dispatch(songActions.changeFilterBy(value)),
     onDurationHoursChanged: value => dispatch(songActions.changeDurationHours(value)),
     onVocalistChanged: value => dispatch(songActions.changeVocalist(value)),
