31bc5c163e6cb648409d39915ec60757ca5fa2d4
Augsorn Chanklad <uppban5@gmail.com>
2018-04-30 11:29:35 +0700

    Fix favorite albums menu

--- src/features/main/Main/Main.js
+++ src/features/main/Main/Main.js
@@ -33,7 +33,7 @@ export default class Main extends React.PureComponent {
         const menus =  [
             { icon: 'ios-log-in', text: 'Sign in', onPress: this.props.onPressSignOut, onlyGuest: true },
             { icon: 'ios-musical-notes', text: 'Favorite songs', onPress: this.props.onPressMenuFavoriteSongs },
-            { icon: 'ios-disc', text: 'Collections', onPress: this.props.onPressMenuFavoriteAlbums, onlyMember: true },
+            { icon: 'ios-disc', text: 'Collections', onPress: this.props.onPressMenuFavoriteAlbums },
             { icon: 'ios-people', text: 'Followed artists', onPress: this.props.onPressMenuFollowArtists },
             { icon: 'ios-globe', text: 'VocaDB website', onPress: () => openLink('https://vocadb.net') },
             { icon: 'logo-github', text: 'About', onPress: () => openLink('https://github.com/VocaDB/VocaDB-App') },
