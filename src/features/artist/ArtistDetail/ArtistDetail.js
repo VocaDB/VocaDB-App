1682ad2fe056cf8997470e21ddea718c6767e85d
up2code <uppban5@gmail.com>
2018-05-05 22:43:02 +0700

    Improve perf artist detail page

--- src/features/artist/ArtistDetail/ArtistDetail.js
+++ src/features/artist/ArtistDetail/ArtistDetail.js
@@ -19,23 +19,30 @@ import { SongRowList } from './../../song/songHOC'
 
 class ArtistDetailPage extends React.Component {
 
+    state = {
+        shouldRender: false
+    }
+
     componentDidMount () {
         const { params } = this.props.navigation.state;
         this.props.fetchArtist(params.id)
+
+        setTimeout(() => { this.setState({ shouldRender: true }) }, 0)
     }
 
     render () {
         const artist = this.props.artist
+
+        if(!artist || !this.state.shouldRender) {
+            return (<View></View>)
+        }
+
         const { params } = this.props.navigation.state;
         const imageUri = images.getArtistUri(params.id)
         const { latestSongs, popularSongs, latestAlbums, popularAlbums, latestEvents } = this.props;
         const hasAlbum = (latestAlbums && latestAlbums.length > 0) || (popularAlbums && popularAlbums.length > 0)
         const hasSong = (latestSongs && latestSongs.length > 0) || (popularSongs && popularSongs.length > 0)
         const hasEvent = (latestEvents && latestEvents.length > 0)
-
-        if(!artist) {
-            return (<View></View>)
-        }
 
         const Section = props => (<View style={[{ marginVertical: 8, paddingHorizontal: 4 },props.style]}>{props.children}</View>)
 
@@ -69,8 +76,8 @@ class ArtistDetailPage extends React.Component {
             </Section>
         )
 
-        const InfoPage = () => (
-            <Content>
+        const renderInfoPage = (
+            <Content tabLabel='Info'>
                 <Cover
                     imageUri={imageUri}
                     title={artist.name}
@@ -97,7 +104,7 @@ class ArtistDetailPage extends React.Component {
 
         return (
             <ScrollableTabView>
-                <InfoPage tabLabel='Info' />
+                {renderInfoPage}
                 {latestSongs && latestSongs.length > 0 && <SongRowList tabLabel='Songs' data={latestSongs} onPressItem={this.props.onPressSong} />}
                 {hasAlbum && <AlbumGridView tabLabel='Albums' albums={latestAlbums} onPressItem={this.props.onPressAlbum} />}
                 {hasEvent && <EventListPage tabLabel='Events' />}
