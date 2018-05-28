f1715ebbebde5b835c7d3d0a07d69bc3187b0fea
Augsorn Chanklad <uppban5@gmail.com>
2018-05-28 14:35:06 +0700

    Fix #39 add field mainPicture for top/recent albums api

--- src/features/album/albumApi.js
+++ src/features/album/albumApi.js
@@ -2,9 +2,9 @@ import api from '../../common/api'
 
 const albumApi = {
     find: params => api.albums.find({ ...params, 'preferAccurateMatches': true }),
-    getRecentAlbums: params => api.albums.newAlbum(params),
-    getTopAlbums: params => api.albums.top(params),
-    getAlbum: (id, params) => api.albums.get(id, { ...params, fields: 'artists,pvs,tags,tracks,webLinks,AdditionalNames,description' })
+    getRecentAlbums: params => api.albums.newAlbum({ ...params, fields: 'MainPicture'}),
+    getTopAlbums: params => api.albums.top({ ...params, fields: 'MainPicture'}),
+    getAlbum: (id, params) => api.albums.get(id, { ...params, fields: 'artists,pvs,tags,tracks,webLinks,AdditionalNames,description,MainPicture' })
 }
 
 export default albumApi
