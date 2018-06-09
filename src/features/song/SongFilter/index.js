b5a9cb7b5be2408a29ecd07a8ed3c8ec75fb0cfa
up2code <uppban5@gmail.com>
2018-06-09 20:53:31 +0700

    Fix song filter add artist #42

--- src/features/song/SongFilter/index.js
+++ src/features/song/SongFilter/index.js
@@ -31,8 +31,8 @@ const songListStateSelect = createSelector(
 const mapDispatchToProps = (dispatch, props) => ({
     onFilterChanged: (params, remove) => dispatch(songActions.fetchSearchSongs(params, remove)),
     onParamChanged: (namve, value) => dispatch(songActions.updateSearchParams(namve, value)),
-    onAddArtist: (artist) => dispatch(songActions.addSearchParamsArray('artistId')),
-    onRemoveArtist: (artist) => dispatch(songActions.removeSearchParamsArray('artistId')),
+    onAddArtist: (artist) => dispatch(songActions.addSearchParamsArray('artistId', artist.id)),
+    onRemoveArtist: (artist) => dispatch(songActions.removeSearchParamsArray('artistId', artist.id)),
     onAddFilterTag: tag => dispatch(songActions.addFilterTag(tag)),
     onSelectFilterTag: tag => dispatch(songActions.addSelectedFilterTag(tag)),
     onDeselectFilterTag: tag => dispatch(songActions.removeSelectedFilterTag(tag)),
