2ac6dc77aa9e6e70fe2f749adb9bfe027a1aa7e3
up2code <uppban5@gmail.com>
2018-06-09 22:45:55 +0700

    Add icon remove to tag

--- src/features/tag/Tag/style.js
+++ src/features/tag/Tag/style.js
@@ -5,6 +5,7 @@ export default {
         paddingHorizontal: 16, paddingVertical: 8,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'center',
         borderRadius: 25
     },
     default: {
