255f847a0a4a0b3235eae6224aed1eddcaa7a192
up2code <uppban5@gmail.com>
2018-06-16 13:34:35 +0700

    Add quick actions #32

--- android/app/src/main/java/com/coolappz/Vocadb/MainApplication.java
+++ android/app/src/main/java/com/coolappz/Vocadb/MainApplication.java
@@ -3,6 +3,7 @@ package com.coolappz.Vocadb;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactNativeQuickActions.AppShortcutsPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppShortcutsPackage(),
             new RNI18nPackage(),
             new VectorIconsPackage()
       );
