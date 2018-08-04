c4cdac4c3ebbb69b5f4769da4b5cddd9162822f0
up2code <uppban5@gmail.com>
2018-08-04 23:16:43 +0700

    Integrate Firebase to android

--- android/app/src/main/java/com/coolappz/Vocadb/MainApplication.java
+++ android/app/src/main/java/com/coolappz/Vocadb/MainApplication.java
@@ -3,6 +3,7 @@ package com.coolappz.Vocadb;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.reactNativeQuickActions.AppShortcutsPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new AppShortcutsPackage(),
             new RNI18nPackage(),
             new VectorIconsPackage()
