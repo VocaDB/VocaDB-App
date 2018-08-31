ee460a10033fd79f6dfdf7d88e6966c428877a69
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2018-08-31 08:53:05 +0700

    Add PVs param to api event published songs

--- src/features/releaseEvent/releaseEventApi.js
+++ src/features/releaseEvent/releaseEventApi.js
@@ -17,7 +17,7 @@ const releaseEventApi = {
         afterDate: daysAgo(3),
         beforeDate: dateAfterToday(12),
         fields: 'WebLinks,Description,Artists,MainPicture,Series,SongList' }),
-    getPublishedSongs: (id, params) => api.events.getPublishedSongs(id, { ...params, fields: 'ThumbUrl', maxResults: 20 }),
+    getPublishedSongs: (id, params) => api.events.getPublishedSongs(id, { ...params, fields: 'ThumbUrl,MainPicture,PVs', maxResults: 20 }),
     getAlbums: (id, params) => api.events.getAlbums(id, { ...params, fields: 'MainPicture' }),
     getSongList: (songListId, params) => api.songLists.songs(songListId, { ...params, fields: 'ThumbUrl', maxResults: 50 })
 }
