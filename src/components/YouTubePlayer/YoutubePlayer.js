80fdf538688f8a3d07b714da4a34f25e76f49926
up2code <uppban5@gmail.com>
2019-03-04 15:07:16 +0700

    #82 Disable Youtube play in background

--- src/components/YouTubePlayer/YoutubePlayer.js
+++ src/components/YouTubePlayer/YoutubePlayer.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import { View, Text, WebView } from 'react-native';
+import { View, Text, WebView, AppState } from 'react-native';
 import PropTypes from 'prop-types';
 
 const Player = require('./player.html');
 
 class YoutubePlayer extends React.Component {
+
+    state = {
+        appState: AppState.currentState
+    }
+  
+    componentDidMount() {
+        AppState.addEventListener('change', this._handleAppStateChange);
+    }
+  
+    componentWillUnmount() {
+        AppState.removeEventListener('change', this._handleAppStateChange);
+    }
+  
+    _handleAppStateChange = (nextAppState) => {        
+        this.setState({appState: nextAppState});
+    }
+  
     render () {
 
         return (
             <View style={{ height: 260 }}>
-                <WebView
-                    style={{flex:1}}
-                    javaScriptEnabled={true}
-                    mediaPlaybackRequiresUserAction={false}
-                    allowsInlineMediaPlayback={true}
-                    source={{uri: `https://www.youtube.com/embed/${this.props.pvId}?playsinline=1`}}
-                />
+
+                {this.state.appState == 'active' &&
+                    <WebView
+                        style={{flex:1}}
+                        javaScriptEnabled={true}
+                        mediaPlaybackRequiresUserAction={true}
+                        allowsInlineMediaPlayback={true}
+                        source={{uri: `https://www.youtube.com/embed/${this.props.pvId}?playsinline=1`}}
+                    />
+                }
+
             </View>
         )
     }
