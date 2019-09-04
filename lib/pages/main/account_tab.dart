008d57e1e091f3eae92b20f94cc1ca1a8ff1da7f
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-09-04 17:18:21 +0700

    Add mock login page

--- lib/pages/main/account_tab.dart
+++ lib/pages/main/account_tab.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:vocadb/pages/login/login_page.dart';
 
 class AccountTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Account tab', textDirection: TextDirection.ltr),
-    );
+    return GuestTab();
   }
 
+}
+
+class GuestTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => LoginPage()));
+          },
+          leading: Icon(Icons.lock),
+          title: Text("Sign in"),
+        ),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text("Setting"),
+        ),
+        ListTile(
+          leading: Icon(Icons.info),
+          title: Text("Version"),
+          subtitle: Text("3.0.0-alpha.1"),
+        )
+      ],
+    );
+  }
 }
