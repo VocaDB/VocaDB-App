5ca46783222142033202ff89367ac1ba5f18ff69
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-09-04 17:53:27 +0700

    Add mock tabbar on ranking page

--- lib/pages/main/ranking_tab.dart
+++ lib/pages/main/ranking_tab.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
 
-class RankingTab extends StatelessWidget {
+class RankingTab extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Ranking tab', textDirection: TextDirection.ltr),
-    );
+  _RankingTabState createState() => _RankingTabState();
+}
+
+class _RankingTabState extends State<RankingTab>
+    with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: "Daily"),
+    Tab(text: "Weekly"),
+    Tab(text: "Monthly"),
+    Tab(text: "Overall")
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: myTabs.length, initialIndex: 1);
   }
 
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> tabChildren = myTabs.map((Tab tab) {
+      final String label = tab.text.toLowerCase();
+      return Center(
+        child: Text(
+          '$label',
+        ),
+      );
+    }).toList();
+
+    return SafeArea(
+      child: Scaffold(
+      appBar: TabBar(controller: _tabController, tabs: myTabs),
+      body: TabBarView(
+        controller: _tabController,
+        children: tabChildren,
+      )
+      )
+    );
+  }
 }
