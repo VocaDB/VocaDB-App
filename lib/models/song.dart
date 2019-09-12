95e08ead77eace372508c26c21c1977e163f16f9
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-09-12 15:35:48 +0700

    Add PVs property

--- lib/models/song.dart
+++ lib/models/song.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:vocadb/models/pv.dart';
 import 'package:vocadb/services/web_service.dart';
 
 class Song {
@@ -7,12 +8,18 @@ class Song {
   String name;
   String artistString;
   String thumbUrl;
+  List<PV> pvs;
+
+  Song();
 
   Song.fromJson(Map<String, dynamic> json) :
     id = json['id'],
     name = json['name'],
     artistString = json['artistString'],
-    thumbUrl = json['thumbUrl'];
+    thumbUrl = json['thumbUrl'],
+    pvs = (json['pvs'] as List<Map<String, dynamic>>)?.map((pv) => PV.fromJson(pv))?.toList();
+
+  PV get youtubePV => pvs?.singleWhere((pv) => pv.service.toLowerCase() == "youtube", orElse: () => null);
 
   static Resource<List<Song>> get all {
     
@@ -21,6 +28,18 @@ class Song {
       parse: (response) {
         Iterable result = json.decode(response.body);
         return result.map((model) => Song.fromJson(model)).toList();
+      }
+    );
+
+  }
+
+  static Resource<Song> byId(int id) {
+    
+    return Resource(
+      url: 'https://vocadb.net/api/songs/$id?fields=PVs,ThumbUrl,Albums,Artists,Tags,WebLinks',
+      parse: (response) {
+        final result = json.decode(response.body);
+        return Song.fromJson(result);
       }
     );
 
