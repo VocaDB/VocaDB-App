513032caa5a1a23f6e70bdc611a11d66b891acba
up2up <uppban5@gmail.com>
2019-10-12 12:21:17 +0700

    Update song search page

---
+++ lib/blocs/song_bloc.dart
@@ -0,0 +1,66 @@
+import 'package:rxdart/rxdart.dart';
+import 'package:vocadb/blocs/config_bloc.dart';
+import 'package:vocadb/blocs/search_song_filter_bloc.dart';
+import 'package:vocadb/models/song_model.dart';
+import 'package:vocadb/services/song_rest_service.dart';
+
+class SongBloc {
+  final _query = BehaviorSubject<String>.seeded(null);
+  final _results = BehaviorSubject<List<SongModel>>.seeded(null);
+  final _searching = BehaviorSubject<bool>.seeded(false);
+
+  Observable get result$ => _results.stream;
+  Observable get query$ => _query.stream;
+  Observable get searching$ => _searching.stream;
+
+  String get query => _query.value;
+
+  SongRestService songService = SongRestService();
+
+  ConfigBloc configBloc;
+
+  SearchSongFilterBloc songFilterBloc;
+
+  SongBloc({this.configBloc, this.songFilterBloc}) {
+    songFilterBloc ??= SearchSongFilterBloc();
+
+    Observable.merge([
+      query$,
+      songFilterBloc.params$,
+    ]).debounceTime(Duration(milliseconds: 500)).distinct().listen(fetch);
+  }
+
+  void openSearch() {
+    _searching.add(true);
+  }
+
+  void updateQuery(String query) {
+    _query.add(query);
+  }
+
+  void updateResults(List<SongModel> results) {
+    _results.add(results);
+  }
+
+  void fetch(dynamic event) {
+    print(event);
+    Map<String, String> params = songFilterBloc.params();
+
+    params['fields'] = 'PVs,MainPicture,ThumbUrl';
+    params['nameMatchMode'] = 'Auto';
+    params['maxResults'] = '50';
+    params['lang'] = configBloc.contentLang;
+
+    if (query != null && query.isNotEmpty) {
+      params['query'] = query;
+    }
+
+    songService.list(params: params).then(updateResults);
+  }
+
+  void dispose() {
+    _query?.close();
+    _results?.close();
+    _searching?.close();
+  }
+}
--- lib/pages/song/song_page.dart
+++ lib/pages/song/song_page.dart
@@ -2,14 +2,16 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vocadb/blocs/config_bloc.dart';
-import 'package:vocadb/blocs/search_song_bloc.dart';
+import 'package:vocadb/blocs/song_bloc.dart';
 import 'package:vocadb/models/song_model.dart';
+import 'package:vocadb/pages/search/search_song_filter_page.dart';
+import 'package:vocadb/pages/youtube_playlist/youtube_playlist_page.dart';
+import 'package:vocadb/widgets/center_content.dart';
 import 'package:vocadb/widgets/result.dart';
 import 'package:vocadb/widgets/song_tile.dart';
 
 class SongScreen extends StatelessWidget {
   static const String routeName = '/songs';
-
 
   static void navigate(BuildContext context) {
     Navigator.pushNamed(context, SongScreen.routeName);
@@ -19,16 +21,32 @@ class SongScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final configBloc = Provider.of<ConfigBloc>(context);
 
-    return Provider<SearchSongBloc>(
-      builder: (context) => SearchSongBloc(configBloc: configBloc),
+    return Provider<SongBloc>(
+      builder: (context) => SongBloc(configBloc: configBloc),
       dispose: (context, bloc) => bloc.dispose(),
       child: SongPage(),
     );
   }
 }
 
-class SongPage extends StatelessWidget {
- Widget buildData(List<SongModel> songs) {
+class SongPage extends StatefulWidget {
+  @override
+  _SongPageState createState() => _SongPageState();
+}
+
+class _SongPageState extends State<SongPage> {
+  final TextEditingController _controller = TextEditingController();
+
+  Widget buildData(List<SongModel> songs) {
+    if (songs.length == 0) {
+      return CenterResult(
+        result: Result(
+          Icon(Icons.search),
+          'No result',
+        ),
+      );
+    }
+
     return ListView.builder(
       itemCount: songs.length,
       itemBuilder: (context, index) {
@@ -36,6 +54,11 @@ class SongPage extends StatelessWidget {
         return SongTile.fromSong(song);
       },
     );
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   Widget buildLeading(String imageUrl) {
@@ -70,35 +93,99 @@ class SongPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final bloc = Provider.of<SearchSongBloc>(context);
+    final bloc = Provider.of<SongBloc>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Songs'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
+        appBar: AppBar(
+          title: StreamBuilder(
+            stream: bloc.searching$,
+            builder: (context, snapshot) {
+              return AnimatedSwitcher(
+                duration: Duration(milliseconds: 100),
+                child: (snapshot.hasData && snapshot.data)
+                    ? Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: TextField(
+                              controller: _controller,
+                              onChanged: bloc.updateQuery,
+                              style: Theme.of(context).primaryTextTheme.title,
+                              autofocus: true,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none, hintText: "Search"),
+                            ),
+                          ),
+                        ],
+                      )
+                    : Text('Songs'),
+              );
+            },
           ),
-          IconButton(
-            icon: Icon(Icons.filter_list),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: StreamBuilder(
-        stream: bloc.result$,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return buildData(snapshot.data);
-          } else if (snapshot.hasError) {
-            return buildError(snapshot.error.toString());
-          }
+          actions: <Widget>[
+            StreamBuilder(
+                stream: bloc.searching$,
+                builder: (context, snapshot) {
+                  return (snapshot.hasData && snapshot.data)
+                      ? IconButton(
+                          icon: Icon(Icons.clear),
+                          onPressed: () {
+                            bloc.updateQuery('');
+                            _controller.clear();
+                          },
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: () => bloc.openSearch(),
+                        );
+                }),
+            IconButton(
+              icon: Icon(Icons.filter_list),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            SearchSongFilterPage(bloc: bloc.songFilterBloc)));
+              },
+            ),
+          ],
+        ),
+        body: StreamBuilder(
+          stream: bloc.result$,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return buildData(snapshot.data);
+            } else if (snapshot.hasError) {
+              return buildError(snapshot.error.toString());
+            }
 
-          return buildDefault();
-        },
-      ),
-    );
+            return buildDefault();
+          },
+        ),
+        floatingActionButton: StreamBuilder(
+          stream: bloc.result$,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Container();
+            }
+
+            List<SongModel> songs = snapshot.data;
+
+            if (songs.indexWhere((s) => s.youtubePV != null) >= 0) {
+              return FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              YoutubePlaylistPage(songs: songs)));
+                },
+                child: Icon(Icons.play_arrow),
+              );
+            }
+
+            return Container();
+          },
+        ));
   }
 }
