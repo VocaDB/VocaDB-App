53afa137133b6f5cb323460735590f400be5dee6
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-12-30 16:45:11 +0700

    Fix update_appfile.sh

--- ios/update_appfile.sh
+++ ios/update_appfile.sh
@@ -4,7 +4,7 @@ APPLE_ID=$2
 TEAM_NAME=$3
 TEAMN_ID=$4
 
-sed -i "s/##APP_IDENTIFIER##/$APP_IDENTIFIER/g" ./fastlane/Appfile
-sed -i "s/##APPLE_ID##/$APPLE_ID/g" ./fastlane/Appfile
-sed -i "s/##TEAM_NAME##/$TEAM_NAME/g" ./fastlane/Appfile
-sed -i "s/##TEAMN_ID##/$TEAMN_ID/g" ./fastlane/Appfile
+sed -i "s/##APP_IDENTIFIER##/$APP_IDENTIFIER/g" fastlane/Appfile
+sed -i "s/##APPLE_ID##/$APPLE_ID/g" fastlane/Appfile
+sed -i "s/##TEAM_NAME##/$TEAM_NAME/g" fastlane/Appfile
+sed -i "s/##TEAMN_ID##/$TEAMN_ID/g" fastlane/Appfile
