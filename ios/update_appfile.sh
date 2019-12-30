eefa1d0c6179bbc5b56b1306b883bdd0dedbac20
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-12-30 10:34:44 +0700

    Add script dynamic change Appfile for ios

---
+++ ios/update_appfile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+APP_IDENTIFIER=$1
+APPLE_ID=$2
+TEAM_NAME=$3
+TEAMN_ID=$4
+
+sed -i "s/##APP_IDENTIFIER##/$APP_IDENTIFIER/g" ./fastlane/Appfile
+sed -i "s/##APPLE_ID##/$APPLE_ID/g" ./fastlane/Appfile
+sed -i "s/##TEAM_NAME##/$TEAM_NAME/g" ./fastlane/Appfile
+sed -i "s/##TEAMN_ID##/$TEAMN_ID/g" ./fastlane/Appfile
