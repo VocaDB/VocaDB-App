93bf8edba44350f617f2c505e9e9f3c5b266dc84
up2up <uppban5@gmail.com>
2020-06-29 15:43:08 +0700

    Clean unused code

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vocadb/bloc/bloc.dart';
 import 'package:vocadb/parameters/parameters.dart';
-import 'package:vocadb/repositories/repositories.dart';
 import 'package:vocadb/shared_widgets/shared_widgets.dart';
 
 class HomePage extends StatelessWidget {
-  SongBloc highlightedBloc;
-
   @override
   Widget build(BuildContext context) {
-    final songRepository =
-        RepositoryProvider.of<VocaDBRepository>(context).songRepository;
     return Scaffold(
       appBar: AppBar(
         title: Text('VocaDB'),
