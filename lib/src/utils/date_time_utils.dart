e400284ba1ab54a0db23140a38fedc29619e13cf
up2up <uppban5@gmail.com>
2020-12-21 17:25:43 +0700

    Fix typo

--- lib/src/utils/date_time_utils.dart
+++ lib/src/utils/date_time_utils.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 
 /// An utility class for manipulate date time value
-class DateTiemUtils {
+class DateTimeUtils {
   /// Convert json array value to list
   static String toSimpleFormat(DateTime value) {
     final DateFormat formatter = DateFormat('yyyy-MM-dd');
