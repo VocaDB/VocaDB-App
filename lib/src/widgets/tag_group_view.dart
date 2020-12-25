9ece1735443a2eac01b3482fce9b397e67aa9170
up2up <uppban5@gmail.com>
2020-12-25 22:49:43 +0700

    Add feature tag group minimum display

--- lib/src/widgets/tag_group_view.dart
+++ lib/src/widgets/tag_group_view.dart
@@ -3,7 +3,7 @@ import 'package:vocadb_app/models.dart';
 import 'package:vocadb_app/widgets.dart';
 
 /// A widget to generate tags by list of tag model.
-class TagGroupView extends StatelessWidget {
+class TagGroupView extends StatefulWidget {
   /// The list of tag models.
   final List<TagModel> tags;
 
@@ -13,20 +13,58 @@ class TagGroupView extends StatelessWidget {
   /// A horizontal margin size. Default is 16.0
   final double margin;
 
-  const TagGroupView({this.tags, this.onPressed, this.margin = 16.0})
+  /// Minimum tags to display. Set to zero to display all
+  final int minimumDisplayCount;
+
+  const TagGroupView(
+      {this.tags,
+      this.onPressed,
+      this.margin = 16.0,
+      this.minimumDisplayCount = 5})
       : assert(tags != null);
 
   @override
+  _TagGroupViewState createState() => _TagGroupViewState();
+}
+
+class _TagGroupViewState extends State<TagGroupView> {
+  bool displayAll = false;
+
+  void _onMorePressed() {
+    setState(() {
+      this.displayAll = true;
+    });
+  }
+
+  Widget _mapTagWidget(TagModel tagModel) {
+    return new Tag(
+        label: tagModel.name,
+        onPressed: () => (this.widget.onPressed != null)
+            ? this.widget.onPressed(tagModel)
+            : null);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    List<Widget> items = [];
+
+    if (widget.minimumDisplayCount > 0 && !displayAll) {
+      items = widget.tags
+          .take(this.widget.minimumDisplayCount)
+          .map(this._mapTagWidget)
+          .toList();
+      items.add(InputChip(
+        label: Text('More (${this.widget.tags.length})'),
+        onPressed: this._onMorePressed,
+      ));
+    } else {
+      items = widget.tags.map(this._mapTagWidget).toList();
+    }
+
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: this.margin),
+      margin: EdgeInsets.symmetric(horizontal: this.widget.margin),
       child: Wrap(
-        children: tags
-            .map((e) => Tag(
-                label: e.name,
-                onPressed: () =>
-                    (this.onPressed != null) ? this.onPressed(e) : null))
-            .toList(),
+        children: items,
       ),
     );
   }
