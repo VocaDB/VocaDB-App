5506e5d058b57053e470e161fe3f9b70546d9446
up2up <uppban5@gmail.com>
2021-01-13 22:40:29 +0700

    Apply search controller to tag page

--- lib/src/controllers/tag_search_controller.dart
+++ lib/src/controllers/tag_search_controller.dart
@@ -1,56 +1,26 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vocadb_app/arguments.dart';
+import 'package:vocadb_app/controllers.dart';
 import 'package:vocadb_app/models.dart';
 import 'package:vocadb_app/repositories.dart';
 import 'package:vocadb_app/services.dart';
 
-class TagSearchController extends GetxController {
-  final int maxResults = 50;
-
-  final initialLoading = true.obs;
-
-  /// List of results from search
-  final results = <TagModel>[].obs;
-
-  /// Query input string
-  final query = ''.obs;
-
+class TagSearchController extends SearchPageController<TagModel> {
   final category = ''.obs;
 
-  /// Set to True when user tap search icon.
-  final openQuery = false.obs;
-
-  /// If set to [True], no fetch more data from server. Default is [False].
-  final noFetchMore = false.obs;
-
   final TagRepository tagRepository;
-
-  TextEditingController textSearchController;
 
   TagSearchController({this.tagRepository});
 
   @override
   void onInit() {
     initArgs();
-    initialFetch();
-    debounce(query, (_) => initialFetch(), time: Duration(seconds: 1));
-    textSearchController = TextEditingController();
     super.onInit();
-  }
-
-  void initialFetch() {
-    Future.value(noFetchMore(false))
-        .then((_) => fetchApi())
-        .then(verifyShouldFetchMore)
-        .then(results)
-        .then(initialLoadingDone);
   }
 
   initArgs() {
     final TagSearchArgs args = Get.arguments;
     category(args.category);
-    print(args);
   }
 
   Future<List<TagModel>> fetchApi({int start}) => tagRepository.findTags(
@@ -59,24 +29,4 @@ class TagSearchController extends GetxController {
       maxResults: maxResults,
       query: query.string,
       categoryName: category.string);
-
-  clearQuery() {
-    query('');
-    textSearchController.clear();
-  }
-
-  initialLoadingDone(_) => initialLoading(false);
-
-  List<TagModel> verifyShouldFetchMore(List<TagModel> items) {
-    if (items == null || items.isEmpty || items.length < maxResults)
-      noFetchMore(true);
-    return items;
-  }
-
-  onReachLastItem() {
-    if (noFetchMore.value) return;
-    fetchApi(start: results.length + 1)
-        .then(verifyShouldFetchMore)
-        .then(results.addAll);
-  }
 }
