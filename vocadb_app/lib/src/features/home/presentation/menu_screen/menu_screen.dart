46911227abe2235aa48a8b50b63252affaa4541b
Augsorn Chanklad <uppban5@gmail.com>
2022-12-25 00:46:25 +0700

    Update menu screen

--- vocadb_app/lib/src/features/home/presentation/menu_screen/menu_screen.dart
+++ vocadb_app/lib/src/features/home/presentation/menu_screen/menu_screen.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:vocadb_app/src/common_widgets/responsive_center.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ResponsiveCenter(
-        child: Text(
-          'Menu',
-          textAlign: TextAlign.center,
-        ),
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.login),
+            title: const Text('Log in'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.email),
+            title: const Text('Contact us'),
+            onTap: () => {},
+          ),
+          const AboutListTile(
+            icon: Icon(Icons.info),
+            applicationIcon: FlutterLogo(),
+            applicationLegalese:
+                'VocaDB is a free to use database for discography information about Vocaloid and related voice synthesizers.',
+            applicationName: 'VocaDB',
+            applicationVersion: 'version 3.1.0-beta.2',
+          ),
+        ],
       ),
     );
   }
