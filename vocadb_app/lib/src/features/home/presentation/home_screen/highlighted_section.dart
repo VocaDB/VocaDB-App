9d0ec711808cc7e7a04f13e976b38ea5850de4da
Augsorn Chanklad <uppban5@gmail.com>
2022-12-25 23:24:00 +0700

    Add songs section widget

--- vocadb_app/lib/src/features/home/presentation/home_screen/highlighted_section.dart
+++ vocadb_app/lib/src/features/home/presentation/home_screen/highlighted_section.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:vocadb_app/src/common_widgets/section.dart';
 import 'package:vocadb_app/src/features/songs/data/song_repository.dart';
-import 'package:vocadb_app/src/features/songs/presentation/songs_list/songs_list_view.dart';
-import 'package:vocadb_app/src/routing/app_router.dart';
+import 'package:vocadb_app/src/features/songs/presentation/songs_list/songs_section.dart';
 
 class HighlightedSection extends StatelessWidget {
   const HighlightedSection({super.key});
@@ -12,16 +9,9 @@ class HighlightedSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final songs = SongRepository.instance.getHighlightedSongsList();
 
-    return Section(
+    return SongsSection(
       title: 'Highlighted',
-      child: SongListView(
-        songs: songs,
-        scrollDirection: Axis.horizontal,
-        onSelect: (song) => {
-          GoRouter.of(context)
-              .pushNamed(AppRoute.songDetail.name, params: {'id': song.id})
-        },
-      ),
+      songs: songs,
     );
   }
 }
---
+++ vocadb_app/lib/src/features/songs/presentation/songs_list/songs_section.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:vocadb_app/src/common_widgets/section.dart';
+import 'package:vocadb_app/src/features/songs/domain/song.dart';
+import 'package:vocadb_app/src/features/songs/presentation/songs_list/songs_list_view.dart';
+import 'package:vocadb_app/src/routing/app_router.dart';
+
+/// A widget for display section of songs as horizontal
+class SongsSection extends StatelessWidget {
+  const SongsSection(
+      {super.key, this.title = 'Songs', required this.songs, this.onSelect});
+
+  final String title;
+
+  final List<Song> songs;
+
+  /// Navigate to song detail screen by default if onSelect not defined
+  final Function(Song)? onSelect;
+
+  @override
+  Widget build(BuildContext context) {
+    return Section(
+      title: title,
+      child: SongListView(
+        scrollDirection: Axis.horizontal,
+        songs: songs,
+        onSelect: (song) {
+          if (onSelect == null) {
+            GoRouter.of(context)
+                .pushNamed(AppRoute.songDetail.name, params: {'id': song.id});
+          } else {
+            onSelect!(song);
+          }
+        },
+      ),
+    );
+  }
+}
